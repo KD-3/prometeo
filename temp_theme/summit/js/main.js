e8e5e089e6ff05e32138c3b5ab22e8e25f1acf0f
Jashpatel1 <patel.5@iitj.ac.in>
2019-12-13 12:55:24 +0530

    updated counter and header

--- temp_theme/summit/js/main.js
+++ temp_theme/summit/js/main.js
@@ -70,11 +70,11 @@
     /*------------------
         CountDown
     --------------------*/
-	$("#countdown").countdown("2019/12/25", function(event) {
+	$("#countdown").countdown("2020/01/24", function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
-    $("#countdown-2").countdown("2019/12/25", function(event) {
+    $("#countdown-2").countdown("2020/01/24", function(event) {
         $(this).html(event.strftime("<div class='cd-time'><span>%w</span> <p>Week</p> </div>" + "<div class='cd-time'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-time'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-time'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-time'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
