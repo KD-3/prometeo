009257cbf5ae7f6233d8c72cddf186c131fcc5aa
Jashpatel1 <patel.5@iitj.ac.in>
2019-12-16 18:57:07 +0530

    added event template(hackathon)

--- temp_theme/summit/js/main.js
+++ temp_theme/summit/js/main.js
@@ -61,12 +61,16 @@
     /*------------------
         CountDown
     --------------------*/
-    $("#countdown").countdown("2020/01/24", function(event) {
+    $("#countdown").countdown("2020/01/25", function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
-    $("#countdown-2").countdown("2020/01/24", function(event) {
-        $(this).html(event.strftime("<div class='cd-time'><span>%w</span> <p>Week</p> </div>" + "<div class='cd-time'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-time'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-time'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-time'><span>%S</span> <p>Seconds</p> </div>"));
+    $("#countdown-2").countdown("2020/01/25 10:00:00", function(event) {
+        $(this).html(event.strftime("<div class='cd-time'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-time'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-time'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-time'><span>%S</span> <p>Seconds</p> </div>"));
+    });
+
+    $("#countdown-3").countdown("2020/01/25 20:00:00", function(event) {
+        $(this).html(event.strftime("<div class='cd-time'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-time'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-time'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-time'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
     /*------------------
