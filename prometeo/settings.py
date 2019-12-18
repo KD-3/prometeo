bf68fdd165ea00af7d761e9bd44825383104b71c
manul_goyal <manul.goyal@gmail.com>
2019-12-18 12:37:18 +0530

    events page add in progress

--- prometeo/settings.py
+++ prometeo/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser'
