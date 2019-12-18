95f6162075ce43b52d5d5091e574a92c75d356b2
manul_goyal <manul.goyal@gmail.com>
2019-12-18 14:10:14 +0530

    events are displayed

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.conf import settings
 from .views import EventsView
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', EventsView, name="events")
-]
+] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- prometeo/urls.py
+++ prometeo/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('events/', include('events.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
