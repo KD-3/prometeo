7a7eec323d8347b23222b82f3395bf3dddee0743
manul_goyal <manul.goyal@gmail.com>
2019-12-18 14:27:22 +0530

    deleted extra folders

--- prometeo/events/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Event
-
-# Create your views here.
-def EventsView(request):
-    events = Event.objects.all()
-    return render(request, 'events.html', {'events' : events})
