1575325ea681b41e62331ab3a5bbef26a3b8d2bd
manul_goyal <manul.goyal@gmail.com>
2020-01-05 16:09:40 +0530

    adjusted message display and logo

--- users/admin.py
+++ users/admin.py
@@ -13,3 +13,4 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username',]
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
--- users/views.py
+++ users/views.py
@@ -7,3 +7,5 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
