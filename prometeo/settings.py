91f4c5b373b427cb2393474b7975fe42d4c98161
Kaustubh Dixit <kdkishu335@gmail.com>
2020-01-09 22:23:33 +0530

    Added IP address

--- prometeo/settings.py
+++ prometeo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'zf0o(fvf3=d-ohx9dmybcw@7i7n#d7xb$nl*&wa66fuw92gb99'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.110', '127.0.0.1',]
+ALLOWED_HOSTS = ['192.168.43.110', '127.0.0.1', '142.93.216.166',]
 
 # Application definition
 
