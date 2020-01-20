b5705aa283856fe8143e146bdf44f3536091f0bf
manul_goyal <manul.goyal@gmail.com>
2020-01-20 20:33:22 +0530

    speakers

--- events/models.py
+++ events/models.py
@@ -21,13 +21,13 @@ class Event(models.Model):
     max_team_size = models.IntegerField(verbose_name="Maximum Team Size (leave unchanged for individual event)", default=5)
     prize = models.IntegerField(verbose_name="Prize Money (Rs.)", null=True, blank=True)
     description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Event Description")
-    problem_statement = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Problem Statement")
     host = models.CharField(max_length=50, null=True, blank=True, verbose_name="Event Host")
     external_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="External Link for Registration")
     date = models.DateField(verbose_name="Event Date")
     time = models.TimeField(null=True, blank=True, verbose_name="Event Time")
     venue = models.CharField(max_length=50, null=True, blank=True, verbose_name="Event Venue")
     type = models.CharField(max_length=15,choices=EVENT_CHOICES,default='event', verbose_name='Event Type')
-    
     def __str__(self):
         return self.name
+
+    
--- speakers/models.py
+++ speakers/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Speaker(models.Model):
     name = models.CharField(max_length=50, verbose_name="Speaker Name")
+    designation = models.CharField(max_length=50, null=True, blank=True, verbose_name="Designation")
     topic = models.CharField(max_length=70, verbose_name="Topic")
     image = models.ImageField(upload_to="images/", null=True, blank=True, verbose_name="Cover Image")
     description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Description")
